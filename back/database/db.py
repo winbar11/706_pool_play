caee16b9f71f972701670752b837c42e939f423f
Windell Barfield <winbar11@gmail.com>
2026-04-02 12:16:10 -0400

    updated psycopg depend

--- back/database/db.py
+++ back/database/db.py
@@ -1,11 +1,11 @@
 import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
+import psycopg
+from psycopg.rows import dict_row
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "")
 
 def get_conn():
-    conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+    conn = psycopg.connect(DATABASE_URL, row_factory=dict_row)
     return conn
 
 def init_db():
